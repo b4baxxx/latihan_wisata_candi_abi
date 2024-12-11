c694ef0705e4aca6f383d924a67836263240817f
b4baxxx <160828060+b4baxxx@users.noreply.github.com>
2024-12-11 07:47:39 +0700

    update
    hero animation update

--- lib/widgets/item_card.dart
+++ lib/widgets/item_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wisata_candi/models/candi.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 
+
 class ItemCard extends StatelessWidget {
   // TODO : 1. Deklarasikan variabel yg dibutuhkan dan pasang pada konstruktor
   final Candi candi;
@@ -14,10 +15,10 @@ class ItemCard extends StatelessWidget {
     return InkWell(
       onTap: () {
         Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) =>DetailScreen(candi: candi),
-            ),
+          context,
+          MaterialPageRoute(
+            builder: (context) =>DetailScreen(candi: candi),
+          ),
         );
       },
       child: Card(
@@ -26,24 +27,27 @@ class ItemCard extends StatelessWidget {
         margin: EdgeInsets.all(4),
         elevation: 1,
         child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // TODO : 3. Buat Image sebagai anak dari Column
-          Expanded(
-            // TODO : 7. Implementasi Hero animation
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.asset(
-                  candi.imageAsset,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // TODO : 3. Buat Image sebagai anak dari Column
+            Expanded(
+              // TODO : 7. Implementasi Hero animation
+              child: Hero(
+                tag: candi.imageAsset,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-          // TODO : 4. Buat Text sebagai anak dari Column
-          Padding(
-            padding: EdgeInsets.only(left: 16, top: 8),
-            child: Text(
+            // TODO : 4. Buat Text sebagai anak dari Column
+            Padding(
+              padding: EdgeInsets.only(left: 16, top: 8),
+              child: Text(
                 candi.name,
                 style: TextStyle(
                   fontSize: 16,
@@ -51,18 +55,18 @@ class ItemCard extends StatelessWidget {
                 ),
               ),
             ),
-          // TODO : 5. Buat Text sebagai anak dari Column
-          Padding(
-            padding: EdgeInsets.only(left: 16, bottom: 8),
-            child: Text(
-              candi.type,
-              style: TextStyle(
-                fontSize: 12,
+            // TODO : 5. Buat Text sebagai anak dari Column
+            Padding(
+              padding: EdgeInsets.only(left: 16, bottom: 8),
+              child: Text(
+                candi.type,
+                style: TextStyle(
+                  fontSize: 12,
                 ),
-             ),
-           ),
-         ],
-       ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
